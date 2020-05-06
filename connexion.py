37b6a0765be7b9033152bb550e4d326e3db07a6c
Vansteenk <62255688+vansteenk@users.noreply.github.com>
2020-05-06 12:01:03 +0200

    Update connexion.py

--- connexion.py
+++ connexion.py
@@ -3,22 +3,16 @@
 # -*- coding: utf-8 -*-
 
 import ftplib
-from getpass import getpass
 
 
 statut=False
 while statut!=True :
 	hostname=input("Hostname : ") # Addresse du serveur FTP
 	username="user"
-	password=getpass()
+	password="testtest"
 	
 	#tentative de connexion
-	try:
-		connect=ftplib.FTP('hostname','username','password') # connect est la variable de connexion
-		statut=True
-		bienvenue=connect.getwelcome() # on récupère le "message de bienvenue"
-		print(bienvenue)
-	
-	except:
-		print("Erreur. Recommencez. ")
-		statut=False
+	connect=ftplib.FTP('hostname','username','password') # connect est la variable de connexion
+	statut=True
+	bienvenue=connect.getwelcome() # on récupère le "message de bienvenue"
+	print(bienvenue)
